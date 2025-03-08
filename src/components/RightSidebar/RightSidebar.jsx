ef0a58d79d9b3da3e01e3fd86ae4adbbcda54992
Abhishek Dhiman <157941512+abhi-shek77@users.noreply.github.com>
2025-03-08 22:57:53 +0530

    remove firebase keys from repo and profile update responsive

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -20,12 +20,12 @@ const RightSidebar = () => {
       <div className="rs-media">
         <p>Media</p>
         <div>
-          <img src={assets.pic1} alt="" />
+          {/* <img src={assets.pic1} alt="" />
           <img src={assets.pic2} alt="" />
           <img src={assets.pic3} alt="" />
           <img src={assets.pic4} alt="" />
           <img src={assets.pic1} alt="" />
-          <img src={assets.pic2} alt="" />
+          <img src={assets.pic2} alt="" /> */}
         </div>
       </div>
       <button onClick={()=>logout()}>Logout</button>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,61 +1,59 @@
-
 import { initializeApp } from "firebase/app";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8uLgG5rK25NutRUmcwQkM8WeVWbdyzfg",
-  authDomain: "chat-app-27-a34b7.firebaseapp.com",
-  projectId: "chat-app-27-a34b7",
-  storageBucket: "chat-app-27-a34b7.firebasestorage.app",
-  messagingSenderId: "651992431539",
-  appId: "1:651992431539:web:7f2dd9c26f8e433ea5a9c6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-const signup = async(username, email, password)=>{
-    try {
-        const res = await createUserWithEmailAndPassword(auth, email, password);
-        const user = res.user;
-        await setDoc(doc(db,'users',user.uid),{
-            id: user.uid,
-            username: username.toLowerCase(),
-            email,
-            name: "",
-            avatar: "",
-            bio: "Hello I'm using chat app",
-            password:password,
-            lastSeen: Date.now()
-        })
-        await setDoc(doc(db,'chats',user.uid),{
-            chatsData:[]
-        })
-    } catch (error) {
-        console.error(error)
-        toast.error(error.code.split("/")[1].split("-").join(" "));
-    }
-}
+const signup = async (username, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await setDoc(doc(db, "users", user.uid), {
+      id: user.uid,
+      username: username.toLowerCase(),
+      email,
+      name: "",
+      avatar: "",
+      bio: "Hello I'm using chat app",
+      lastSeen: Date.now(),
+    });
+    await setDoc(doc(db, "chats", user.uid), {
+      chatsData: [],
+    });
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
 
-const login = async(email,password)=>{
-    try {
-        await signInWithEmailAndPassword(auth,email,password);
-    } catch (error) {
-        console.error(error)
-        toast.error(error.code.split("/")[1].split("-").join(" "));
-    }
-}
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
 
-const logout = async ()=>{
-    try {
-        await signOut(auth)
-    } catch (error) {
-        console.error(error)
-        toast.error(error.code.split("/")[1].split("-").join(" "));
-    }
-}
+const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
 
-export {signup, login, logout, auth, db}
+export { signup, login, logout, auth, db };
